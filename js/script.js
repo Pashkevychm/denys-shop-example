1deb87c729ec9853006cebe922f5cd5c721ca11f
varash_logika_school <logikavarash@gmail.com>
2025-12-07 15:30:46 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -31,16 +31,58 @@ function renderProducts(items) {
 
     items.forEach(function(item) {
         let productHTML = `
-            <article class="product">
+            <article class="product" data-id="${item.id}">
                 <img src="${item.image}" alt="" class="product-img">
                 <h3 class="product-title">${item.name}</h3>
                 <p class="product-description">${item.description}</p>
                 <p class="product-price"><strong>${item.price} грн</strong></p>
-                <button type="button" class="btn btn-primary">До кошику</button>
+                <button type="button" class="btn btn-primary add-to-cart-btn">До кошику</button>
             </article>
         `
         productsContainer.innerHTML += productHTML
     })
 }
 
+function applyFilters(categoryType) {
+    if (categoryType == "all") {
+        renderProducts(products)
+    } else {
+        let filteredProducts = products.filter(product => product.type == categoryType);
+        renderProducts(filteredProducts)
+    }
+}
+
+function addToCart(productId) {
+    let product = products.find(p => p.id == productId);
+    if (product) {
+        cart.push(product);
+        alert("Товар додано " + product.name)
+        // тут буде збереження в cookies or localStorage
+    }
+}
+
+let productsMap = {
+    "Всі": "all",
+    "Телефони": "phone",
+    "Ноутбуки": "laptop"
+}
+
+function setupFilterButtons() {
+    for (let button of btnGroup.children) {
+        button.addEventListener("click", function() {
+            let category = productsMap[button.innerHTML]
+            applyFilters(category)
+        })
+    }
+}
+
+productsContainer.addEventListener("click", function(event) {
+    if (event.target.classList.contains('add-to-cart-btn')) {
+        let productCard = event.target.closest('.product');
+        let productId = parseInt(productCard.dataset.id)
+        addToCart(productId)
+    }
+})
+
 renderProducts(products)
+setupFilterButtons()
